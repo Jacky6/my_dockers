b98fd1f1e63d8be6b8b78a5a756472142974da25
ququ <1599564164@qq.com>
2024-07-07 10:23:09 +0800

    dev

--- build.sh
+++ build.sh
@@ -1,4 +1,51 @@
 #!/bin/bash
+# 功能: 构建基础镜像
+# author：jackyzheng
+# modified：2024-07-05
+# 
 
-docker build -t dev_base:v1 .
+# set env
+base_version=v1
+dev_version=v0.1
+type=""
+function use_age(){
+    echo "sh build.sh"
+    echo ""
+}
+
+while getopts t:h: opt; do
+    case $opt in
+        t)
+            type=$OPTARG
+            ;;
+        h)
+            use_age
+            exit 0
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+            ;;
+    esac
+done
+
+
+case $type in
+    dev)
+        echo "dev"
+        docker build -t ocr_dev:${dev_version} -f Dockerfile.dev .
+        ;;
+    base)
+        echo "base"
+        docker build -t base:${base_version} -f Dockerfile.base .
+        ;;
+    *)
+        echo "type error"
+        ;;
+esac
+
 
